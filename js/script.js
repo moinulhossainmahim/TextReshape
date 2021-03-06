f6d102095b7a7b7dff0a6516340abf6f9cd7165c
moinulhossainmahim <moinulhossainmahim@gmail.com>
2021-03-06 16:27:11 +0600

    Added javascript file for uppercase html

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 (function() {
 
-    const uppercaseInputText  =  document.getElementById('input-text')
-    const uppercaseOutputText =  document.getElementById('output-text')
+    const uppercaseInputText  = document.getElementById('input-text')
+    const uppercaseOutputText = document.getElementById('output-text')
     const clearButton         = document.getElementById('clear-button')
+    const copyButton          = document.getElementById('copy-button')
 
     clearButton.addEventListener('click', clearInput)
+    copyButton.addEventListener('click', copyToClipboard)
     uppercaseInputText.addEventListener('input', showOutput)
 
     //Convert Text To Uppercase Function
@@ -13,6 +15,16 @@
     }
 
     //Clear Function
-   
+    function clearInput() {
+        uppercaseInputText.value = ''
+        uppercaseOutputText.value = ''
+    }
+
+    //Copy Function 
+    function copyToClipboard() {
+        uppercaseOutputText.select()
+        uppercaseOutputText.setSelectionRange(0, 99999)
+        document.execCommand("copy")
+    }
 
 })();
