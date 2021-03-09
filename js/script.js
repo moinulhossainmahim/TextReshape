0c343a72ac989fde116b70a1b2fc6b02d326f673
moinulhossainmahim <moinulhossainmahim@gmail.com>
2021-03-09 15:43:24 +0600

    Added card width in media queries

--- js/script.js
+++ js/script.js
@@ -1,19 +1,5 @@
 (function() {
 
-
-    // const toKebabCase = str =>
-    // str &&
-    // str
-    //     .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
-    //     .map(x => x.toLowerCase())
-    //     .join('-');
-
-    // console.log(toKebabCase('camelCase'));
-    // console.log(toKebabCase('some text'));
-    // console.log(toKebabCase('some-mixed_string With spaces_underscores-and-hyphens'));
-    // console.log(toKebabCase('AllThe-small Things'));
-    // console.log(toKebabCase('IAmListeningToFMWhileLoadingDifferentURLOnMyBrowserAndAlsoEditingSomeXMLAndHTML'));
-
     const input  = document.getElementById('input')
     const output = document.getElementById('output')
     const clear  = document.getElementById('clear-btn')
@@ -23,7 +9,7 @@
     copy.addEventListener('click', copyToClipboard)
 
     //Clear Function
-    function clearFunction() {
+    function clearFunction(e) {
         input.value = ''
         output.value = ''
     }
